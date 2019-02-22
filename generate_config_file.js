1e2fbfe660f9b066aaa4b047a9cdcd54c256e39f
Quan Tran <quantran@wakumo.vn>
2019-02-22 12:01:55 +0700

    update config

--- generate_config_file.js
+++ generate_config_file.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 (function () {
   let config = {
     "version": "5.0.0-beta.44",
-    "network": process.env.BITCOIN_NETWORK || "testnet",
+    "network": process.env.BITCOIN_NETWORK || "livenet",
     "port": 3000,
     "services": [
       "web",
@@ -21,11 +21,11 @@ const fs = require("fs");
       "bitcoind": {
         "connect": [
           {
-            "rpchost": process.env.BITCOIND_HOST || "35.200.43.24",
-            "rpcport": process.env.BITCOIND_PORT || 18332,
+            "rpchost": process.env.BITCOIND_HOST,
+            "rpcport": process.env.BITCOIND_PORT || (process.env.BITCOIN_NETWORK === "testnet" ? 18332 : 8332),
             "rpcuser": process.env.BITCOIND_USER || "bitcoin-rpc",
             "rpcpassword": process.env.BITCOIND_PASS || "bitcoin-rpc-pass",
-            "zmqpubrawtx": process.env.BITCOIND_ZMQ || "tcp://35.200.43.24:38832"
+            "zmqpubrawtx": process.env.BITCOIND_ZMQ
           }
         ]
       }
