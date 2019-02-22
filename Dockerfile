35d38b31c26ba3c2c0cbb945f844ec91c358b3a9
Quan Tran <quantran@wakumo.vn>
2019-02-22 11:20:32 +0700

    bitcore

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM node:8.2.0-alpine
+#
+ENV APP_ROOT /app
+
+# Create server directory
+WORKDIR $APP_ROOT
+COPY . $APP_ROOT
+
+RUN apk update && apk upgrade && \
+    apk add --no-cache bash git openssh python python-dev py-pip build-base
+RUN apk add --no-cache zeromq-dev
+
+ENV npm_config_zmq_external="true"
+
+RUN npm install
+RUN node generate_config_file
+
+EXPOSE 3000
+CMD ["./bin/bitcore-node", "start"]
---
+++ generate_config_file.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+
+(function () {
+  let config = {
+    "version": "5.0.0-beta.44",
+    "network": process.env.BITCOIN_NETWORK || "testnet",
+    "port": 3001,
+    "services": [
+      "web",
+      "insight-api",
+      "bitcoind"
+    ],
+    "servicesConfig": {
+      "insight-api": {
+        "cwdRequirePath": "node_modules/insight-api",
+        "requirePath": "node_modules/insight-api",
+      },
+      "insight-ui": {
+        "cwdRequirePath": "node_modules/insight-ui"
+      },
+      "bitcoind": {
+        "connect": [
+          {
+            "rpchost": process.env.BITCOIND_HOST || "35.200.43.24",
+            "rpcport": process.env.BITCOIND_PORT || 18332,
+            "rpcuser": process.env.BITCOIND_USER || "bitcoin-rpc",
+            "rpcpassword": process.env.BITCOIND_PASS || "bitcoin-rpc-pass",
+            "zmqpubrawtx": process.env.BITCOIND_ZMQ || "tcp://35.200.43.24:38832"
+          }
+        ]
+      }
+    }
+  };
+  return fs.writeFileSync("bitcore-node.json", JSON.stringify(config, null, 2));
+})();
